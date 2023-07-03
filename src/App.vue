895ec604840ed47fa2be02a263e227e912e97e46
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 10:02:09 +0600

    Episode 05 Code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-const message = 'Welcome. Get ready to master Vue.js 3!'
-const date = '30th June, 2023'
-function getDate(){
-  const date = new Date();
-  return date.toDateString()
-}
+
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl"> {{ message }} </h1>
-    <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ getDate() }}</span></h2>
+      <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
+      <h2 class="mt-10 text-xl text-gray-700">Today is <span>30th June, 2023</span></h2>
   </div>
 </template>
 
