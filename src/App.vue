321a24e8fa8d3532b36a08c056f86a9237cf607b
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 10:25:34 +0600

    Episode 07 Code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-const message = 'Welcome. Get ready to master Vue.js 3!'
-const date = '30th June, 2023'
-
-const data = {
-  message: 'Welcome. Get ready to master Vue.js 3!',
-  date: '30th June, 2023',
-  tasks: [1, 2, 3, 4, 5]
-}
+// import {data} from './data/data'
+import data from './data/data'
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
     <h1 class="text-4xl"> {{ data.message }} </h1>
     <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ data.date }}</span></h2>
-    <h2 class="mt-10 text-xl text-gray-700">You have <span>{{ data.tasks.length }} tasks for today</span></h2>
   </div>
 </template>
 
---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+const data = {
+    message: 'Welcome. Get ready to master Vue.js 3!',
+    date: '30th June, 2023',
+}
+
+// export {data}
+export default data
