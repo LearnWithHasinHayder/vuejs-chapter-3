8f6d8a4e5906c8088e132bc87eeecbd07f58dddd
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 10:46:41 +0600

    Assignment 01

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,20 @@
-const data = {
-    message: 'Welcome. Get ready to master Vue.js 3!',
-    date: '30th June, 2023',
-}
-
-// export {data}
-export default data
+const people = [
+    {
+        image:'https://images.generated.photos/zRuly38LuHo-upVQfxZjUb6YXOHXapy58gXXFglv1Rg/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDAzMzE4LmpwZw.jpg',
+        name: 'Jane Doe',
+        designation: 'CEO, Doe Corporation',
+        location: '968 Ocala Street, Orlando, Florida'
+    },
+    {
+        image:'https://images.generated.photos/dVrq4AXS8SmlQ_HTyxG4bJRmi4IulUA4omUbvvSyQMU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDkwODEzLmpwZw.jpg',
+        name: 'John Doe',
+        designation: 'CTO, Doe Corporation',
+        location: '2970 Duke Lane, Brunswick, New Jersey'
+    },
+    {
+        image:'https://images.generated.photos/XkTVulI9UnbMVl1veOfrra6bS2v0QIqLbSIQrgXni14/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTYxODM1LmpwZw.jpg',
+        name: 'Jimmy Doe',
+        designation: 'CMO, Doe Corporation',
+        location: '3679 Elk Street, Denver, Colorado'
+    }
+]
