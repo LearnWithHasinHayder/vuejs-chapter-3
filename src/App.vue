996511d8d4be51d649e1fa5c9ed1a7255b3c0e08
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 10:17:53 +0600

    Episode 06 Code

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
 const message = 'Welcome. Get ready to master Vue.js 3!'
 const date = '30th June, 2023'
-function getDate(){
-  const date = new Date();
-  return date.toDateString()
+
+const data = {
+  message: 'Welcome. Get ready to master Vue.js 3!',
+  date: '30th June, 2023',
+  tasks: [1, 2, 3, 4, 5]
 }
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl"> {{ message }} </h1>
-    <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ getDate() }}</span></h2>
+    <h1 class="text-4xl"> {{ data.message }} </h1>
+    <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ data.date }}</span></h2>
+    <h2 class="mt-10 text-xl text-gray-700">You have <span>{{ data.tasks.length }} tasks for today</span></h2>
   </div>
 </template>
 
