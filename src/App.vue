22fe6a6fb7cda43a945fe9e9a1bcb6f59dcbdb41
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 12:27:08 +0600

    Episode 10 Code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-const image = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-const title = 'Water surrounded by trees and mountains'
-const link = 'https://unsplash.com/photos/NRQV-hBF10M'
+const image = {
+    src: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    title: 'Water surrounded by trees and mountains',
+    alt: 'Water surrounded by trees and mountains',
+    link: 'https://unsplash.com/photos/NRQV-hBF10M',
+}
 </script>
 
 <template>
@@ -9,9 +12,9 @@ const link = 'https://unsplash.com/photos/NRQV-hBF10M'
         <h1 class="text-4xl mb-10 text-left">Data Project</h1>
         <section class="main flex space-x-4">
             <div class="flex space-y-4 flex-col w-1/2">
-                <img :title="title" :src="image" :alt="title">
-                <h2 class="text-xl text-left"> {{ title }}</h2>
-                <a target="_blank" :href="link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
+                <img v-bind="image">
+                <h2 class="text-xl text-left"> {{ image.title }}</h2>
+                <a target="_blank" :href="image.link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
                     Image Source
                 </a>
             </div>
