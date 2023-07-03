5d71b2ca2ec0330322e63b070a4a6d4b695b491c
Hasin Hayder <countdraculla@gmail.com>
2023-07-03 10:03:43 +0600

    Episode 03 Code

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@
 </script>
 
 <template>
-  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-      <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
-      <h2 class="mt-10 text-xl text-gray-700">Today is <span>30th June, 2023</span></h2>
-  </div>
+
 </template>
 
 <style scoped>
