1e286937f098df748f33d8ffb19f330d652f6519
Hasin Hayder <countdraculla@gmail.com>
2023-07-04 10:34:20 +0600

    Episode 13 Code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+    const message = 'Welcome. Get ready to master <em>Vue.js 3!</em>'
+    const date = '<strong>30th June, 2023</strong>'
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-      <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
-      <h2 class="mt-10 text-xl text-gray-700">Today is <span>30th June, 2023</span></h2>
+      <h1 class="text-4xl" v-html="message"></h1>
+      <h2 class="mt-10 text-xl text-gray-700">Today is <span v-html="date"></span></h2>
   </div>
 </template>
 
